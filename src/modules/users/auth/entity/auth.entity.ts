a379254d96621d4f56dcba5f4ebe1abd71078b8f
Ivan5ik <lacoste545438@gmail.com>
2023-05-19 16:50:20 +0300

    add new models

---
+++ src/modules/users/auth/entity/auth.entity.ts
@@ -0,0 +1,20 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'my-user',
+  timestamps: true,
+  underscored: true,
+})
+export class Auth extends Model<Auth> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+}
